48150820086e2b644bcd626c66a27b638d352b26
RoryPeterson06 <petrg003@mymail.unisa.edu.au>
2026-06-29 23:23:27 +0930

    created abstract class for the Pieces to inheartfrom

--- piece.cpp
+++ piece.cpp
@@ -1,22 +1,43 @@
 #include <string>
-#include 
+#include <vector>
+#include <stdexcept>
 class Piece {
 private:
     // eg E4
     std::string position;
     // eg K - King
     std::string key;
-
-
-
-
-
+    std::vector<std::string> possibleMoves;
 
 public:
 
-
     // Destructer
     virtual ~Piece() {}
+
+    virtual void CreatePossibleMoves()  = 0; 
+
+    std::vector<std::string> getPossibleMoves() {
+        return possibleMoves;
+    }
+
+    std::string getLocation() {
+        return position;
+    }
+
+    void Move(std::string newPosition) {
+        if (std::find(possibleMoves.begin(), possibleMoves.end(), newPosition) != possibleMoves.end()) {
+            position = newPosition;
+        }
+        else {
+            throw std::invalid_argument("Invalid move" + key + " from " + position + " to " + newPosition);
+        }
+    }
+
+    std::string GetNotation() {
+        return key + position;
+    }
+
+
 
 
 };
