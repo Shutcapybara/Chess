1c2db0d9e0fa234eebbbc11faadcb4653db66e7c
RoryPeterson06 <petrg003@mymail.unisa.edu.au>
2026-06-29 23:06:08 +0930

    first commit

---
+++ piece.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include 
+class Piece {
+private:
+    // eg E4
+    std::string position;
+    // eg K - King
+    std::string key;
+
+
+
+
+
+
+public:
+
+
+    // Destructer
+    virtual ~Piece() {}
+
+
+};
